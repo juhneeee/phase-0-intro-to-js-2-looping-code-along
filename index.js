c172c1340058f2eabf9fd7acc329b4969a0d00c6
Johnny Chen <johnnyc1314@gmail.com>
2022-04-05 16:57:47 -0400

    commit tree b0a3f1edc5a4a370c8e94f82cdc1755d34738e73

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(arr){
+    let tempArr=[]
+    for (let i=0; i<arr.length; i++) {
+        tempArr.push(`Thank you, ${arr[i]}, for the wonderful surprise gift!`)
+    }
+    return tempArr
+}
+function writecards2(arr){
+}
+function countDown(num){
+    while (num>= 0){
+        console.log(num)
+        num--
+    }
+}
